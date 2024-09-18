4bd775f8f323ede8379c3a0c6ad01702db5281dc
Wahyu Setiawan <whysetiawan27@gmail.com>
2024-09-18 15:14:03 +0700

    Initial Commit Project with React 19 Compiler

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins: [
+      [
+        'babel-plugin-react-compiler',
+        {
+          runtimeModule: 'react-compiler-runtime',
+        },
+      ], // must run first!
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
