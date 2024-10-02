3bd0325d3af540b93715ab8855032dee593f9220
Wahyu Setiawan <32702491+whysetiawan@users.noreply.github.com>
2024-10-02 15:45:03 +0700

    fix:

--- app/+html.tsx
+++ app/+html.tsx
@@ -19,6 +19,8 @@ export default function Root({ children }: PropsWithChildren) {
         */}
         <ScrollViewStyleReset />
 
+        {/* Add any additional <head> elements that you want globally available on web... */}
+
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
         <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
